af54eb3e9c154bed404f9b9f0f85b43563f4c907
noegoman100 <38665607+noegoman100@users.noreply.github.com>
2020-04-07 00:17:44 -0600

    Initial Commit.

---
+++ src/mouthamatic/Word.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mouthamatic;
+
+/**
+ *
+ * @author Ed
+ */
+public class Word {
+    private String word;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+    
+    
+}
