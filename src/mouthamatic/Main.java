8701c6422b8806e4730eb65139f45cfe0773659c
noegoman100 <38665607+noegoman100@users.noreply.github.com>
2020-07-30 18:13:50 -0600

    Complete. Adding Comments.

--- src/mouthamatic/Main.java
+++ src/mouthamatic/Main.java
@@ -15,7 +15,20 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author Ed
+ * This is the amazing Mouthamatic application. The primary classes in the application are:
+ * SentenceParser, WordParser, SymbolMapper, and ImageMapper. Also, the MySQL database, hosted on AWS,
+ * is the real hero in the story (Mouthamatic V2 uses an embedded SQLite database). The other classes are either helpers,
+ * or just created to fill out all the requirements for my senior project.
+ *
+ * This is a Java 8 application using FXML for the GUI. This was developed with the IntelliJ IDEA IDE
+ * and the SceneBuilder application (built into IDEA). The IDEA Configuration is simple: "Application" from the
+ * templates. The SQL Driver should be in 'lib' folder ("mysql-connector-java-8.0.19").
+ *
+ * The core process is fairly straight forward. Take a raw input sentence. Parse it into individual words.
+ * Parse each word into its constituent symbolIDs (these represent the phonemes). Map each symbol to
+ * the chosen image-set image. Map each image URL to its actual file, and copy out to an export folder.
+ *
+ * Documentation can be found in the Documentation folder
  */
 public class Main extends Application {
     public static DB db = new DB(); 
